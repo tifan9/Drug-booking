39094d9925550bd31411ff88ba741c355f5aa76c
tifan9 <uwasesophie91@gmail.com>
2024-10-23 00:34:45 +0200

    drug booking app

---
+++ src/test/java/com/drug/drugbooking/DrugBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drug.drugbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
